e7ba90468f5da389abb52eded531773e4324be0c
raouldzodoum <rdzodoum123@gmail.com>
2024-05-20 23:15:20 -0400

    module project

---
+++ modules/ec2/vars.tf
@@ -0,0 +1,15 @@
+
+
+  variable "ec2_count" {
+      default = "1"
+  }
+
+  variable "ami_id" {
+      default = "ami-0bb84b8ffd87024d8"
+  }
+
+    variable "instance_type" {
+      default = "t2.micro"
+  }
+
+    variable "subnet_id" {}
